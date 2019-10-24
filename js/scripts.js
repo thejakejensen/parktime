3020c2513cf89dc0e5c2317ff1da30465cb9499d
Student <example@example.com>
2019-10-24 11:49:57 -0700

    add show and hde function to button on coaster page

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,28 @@
 $(document).ready(function(){
   $("#age-checker-btn").click(function(){
-    console.log("made it to function")
-  var age = parseInt(prompt("How old are you?"));
-  if (age <= 7) {
-  $(".main-page").hide();
-  $(".riderip1").toggle();
-} else if (age <= 17) {
-  $(".riderip2").toggle();
-  $(".riderip1").toggle();
-  $(".main-page").hide();
-} else if (age >= 18){
-  $(".riderip3").toggle();
-  $(".riderip2").toggle();
-  $(".riderip1").toggle();
-  $(".main-page").hide();
-}
-else {
-  alert("go to a diffrent park")
-}
+    var age = parseInt(prompt("How old are you?"));
+    if (age <= 7) {
+      $(".main-page").hide();
+      $(".riderip1").toggle();
+    } else if (age <= 17) {
+      $(".riderip2").toggle();
+      $(".riderip1").toggle();
+      $(".main-page").hide();
+    } else if (age >= 18){
+      $(".riderip3").toggle();
+      $(".riderip2").toggle();
+      $(".riderip1").toggle();
+      $(".main-page").hide();
+    }
+    else {
+      alert("go to a diffrent park")
+    }
   });
 
-
-
+  $("#return").click(function(){
+    $(".riderip3").hide();
+    $(".riderip2").hide();
+    $(".riderip1").hide();
+    $(".main-page").show();
+  });
 });
